a4cc62fe766d5dd345b5346db7415a1f7bb26b12
gudanrenao <zhangwenit@126.com>
2019-03-07 02:14:39 +0800

    feign fallback 异常处理

--- spring-cloud-feign-consumer-demo/src/main/java/com/spring/microservice/feign/consumer/service/UserServiceFeign.java
+++ spring-cloud-feign-consumer-demo/src/main/java/com/spring/microservice/feign/consumer/service/UserServiceFeign.java
@@ -3,9 +3,9 @@ package com.spring.microservice.feign.consumer.service;
 import com.spring.microservice.feign.common.api.UserService;
 import com.spring.microservice.feign.common.dto.User;
 import com.spring.microservice.feign.consumer.log.FeignLogConfig;
-import feign.hystrix.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @Description
@@ -14,34 +14,60 @@ import org.springframework.stereotype.Component;
  * @Version 1.0
  **/
 @FeignClient(value = "feign-provider-demo",
-        fallbackFactory = UserServiceFeign.UserServiceFeignFallbackFactory.class,
-//        fallback = UserServiceFeign.UserServiceFallback.class,//服务降级
+//        fallbackFactory = UserServiceFeign.UserServiceFeignFallbackFactory.class,//服务降级
+        fallback = UserServiceFeign.UserServiceFallback.class,//服务降级
         configuration = {FeignLogConfig.class} //Feign调用日志
 )
 public interface UserServiceFeign extends UserService {
 
-
     @Component
-    class UserServiceFeignFallbackFactory implements FallbackFactory<UserServiceFeign> {
+    /**
+     * 注意！！！！！！！！！！！！！！
+     * 这里如果使用fallback的方式，需要更改RequestMapping，否则会报RequestMappingHandlerMapping重复
+     *  Error creating bean with name 'requestMappingHandlerMapping' defined in ...
+     * Ambiguous mapping. Cannot map '...UserServiceFeign' method
+     * 建议使用fallbackFactory的方式
+     */
+    @RequestMapping("/fallback/user")
+    class UserServiceFallback implements UserServiceFeign {
         @Override
-        public UserServiceFeign create(Throwable cause) {
-            return new UserServiceFeign() {
+        public String hello() {
+            return "服务降级 hello";
+        }
 
-                @Override
-                public String hello() {
-                    return "服务降级 hello";
-                }
+        @Override
+        public User findByName(String name) {
+            return new User("服务降级-findByName", -1);
+        }
 
-                @Override
-                public User findByName(String name) {
-                    return new User("服务降级-findByName", -1);
-                }
-
-                @Override
-                public User header(int age, User user) {
-                    return new User("服务降级-Header", age);
-                }
-            };
+        @Override
+        public User header(int age, User user) {
+            return new User("服务降级-Header", age);
         }
     }
+
+
+//    @Component
+//    class UserServiceFeignFallbackFactory implements FallbackFactory<UserServiceFeign> {
+//        @Override
+//        public UserServiceFeign create(Throwable cause) {
+//            return new UserServiceFeign() {
+//
+//                @Override
+//                public String hello() {
+//                    return "服务降级 hello";
+//                }
+//
+//                @Override
+//                public User findByName(String name) {
+//                    return new User("服务降级-findByName", -1);
+//                }
+//
+//                @Override
+//                public User header(int age, User user) {
+//                    return new User("服务降级-Header", age);
+//                }
+//            };
+//        }
+//    }
 }
